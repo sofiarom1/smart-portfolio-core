b2ba332ddd1f0158be71011a1264e06cfe8f4f86
Laura Sofia Romero <laurasofiaromero1003@gmail.com>
2026-02-19 18:49:32 -0500

    se cambiaron los parametros del objeto instrumento en la prueba

--- test/conftest.py
+++ test/conftest.py
@@ -5,7 +5,7 @@ from src.portafolio import Portafolio
 
 @pytest.fixture
 def instrumento_test():
-    return Instrumento(ticker="TSLA", nombre="Tesla")
+    return Instrumento(ticker="TSLA", tipo="Tesla", sector= "tecnologia")
 
 @pytest.fixture
 def portafolio_vacio():
